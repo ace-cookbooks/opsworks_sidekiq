8269b0d62d702ded4c3a135e27bc14781ff05551
Ryan Schlesinger <ryan@instanceinc.com>
2014-02-13 21:15:26 -0800

    Don't trust god's environment for rbenv

--- recipes/default.rb
+++ recipes/default.rb
@@ -12,6 +12,7 @@ node[:deploy].each do |application, deploy|
   end
 
   pid_file = File.join(deploy[:deploy_to], 'shared', 'pids', 'sidekiq.pid')
+  environment_str = deploy[:environment].to_hash.reduce([]){|ary,env| ary << "#{env.first}=#{env.last}"}.join(' ')
   god_monitor 'sidekiq' do
     template_cookbook 'god'
     template_source 'god_monitor.erb'
@@ -21,8 +22,8 @@ node[:deploy].each do |application, deploy|
       :env => deploy[:environment],
       :uid => deploy[:user],
       :dir => deploy[:current_path],
-      :start => "cd #{deploy[:current_path]}; #{File.join(deploy[:current_path], 'bin', 'sidekiq')} -C config/sidekiq.yml -e #{deploy[:rails_env]} -P #{pid_file} 2>&1 | logger -t sidekiq",
-      :stop => "cd #{deploy[:current_path]}; #{File.join(deploy[:current_path], 'bin', 'sidekiqctl')} stop #{pid_file} 60",
+      :start => "cd #{deploy[:current_path]}; /usr/bin/env -i #{environment_str} #{File.join(deploy[:current_path], 'bin', 'sidekiq')} -C config/sidekiq.yml -e #{deploy[:rails_env]} -P #{pid_file} 2>&1 | logger -t sidekiq",
+      :stop => "cd #{deploy[:current_path]}; /usr/bin/env -i #{environment_str} #{File.join(deploy[:current_path], 'bin', 'sidekiqctl')} stop #{pid_file} 60",
       :memory_max => (1434 * 1024)
     })
   end
